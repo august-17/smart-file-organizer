6c243b0ff667387ec4f75f7f7cde39ce3e71e729
August <augustkumarsasmal45@gmail.com>
2026-06-14 18:33:47 +0530

    Read files from current directory

--- organizer.py
+++ organizer.py
@@ -1,1 +1,11 @@
-print("Hello Smart File Organizer")
+import os
+
+curr_folder = os.getcwd()
+
+print("Current Folder:")
+print(curr_folder)
+
+print("\nFiles and Folders:")
+
+for item in os.listdir(curr_folder):
+    print(item)
