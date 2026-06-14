a7b649376d6026aabaa0cbe935d81cc84490e2db
August <augustkumarsasmal45@gmail.com>
2026-06-14 20:26:24 +0530

    Extract file extensions

--- organizer.py
+++ organizer.py
@@ -2,10 +2,10 @@ import os
 import tkinter as tk
 from tkinter import filedialog
 
-root = tk.Tk()                              # Create the root window
-root.withdraw()                             # Hide the root window
+root = tk.Tk()                                      # Create the root window
+root.withdraw()                                     # Hide the root window
 
-folder_path = filedialog.askdirectory()     # Open a dialog to select a folder and store its path
+folder_path = filedialog.askdirectory()             # Open a dialog to select a folder and store its path
 
 if not folder_path:
     print("No folder selected.")
@@ -13,5 +13,17 @@ if not folder_path:
 
 print("Selected:", folder_path)
 
-for item in os.listdir(folder_path):        # List all items in the selected folder
-    print(item)
+for item in os.listdir(folder_path):                # List all items in the selected folder
+
+    item_path = os.path.join(folder_path, item)
+
+    if os.path.isfile(item_path):
+       
+       file_name, extension = os.path.splitext(item)
+       
+       print("File Name:", file_name)
+       print("Extension:", extension)
+       print("-" * 25)
+
+    else:
+        print(item, "-> Folder")
